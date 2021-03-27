5e05e9540ce5765720a9202a6ad5772fff20bf00
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-03-27 15:39:23 +0000

    YahScraper

--- stockBot/helpers/algorithms.py
+++ stockBot/helpers/algorithms.py
@@ -1,10 +1,15 @@
 import abc 
 from abc import ABC, abstractmethod
-import sys
-print(sys.path)
+
+import pandas as pd
+from pandas_datareader import data
+
+from collections import defaultdict
 
 import requests
 from bs4 import BeautifulSoup
+
+from datetime import datetime, timedelta
 
 from config.config import getConfig
 from stockBot.helpers.dataHandler import DataHandler, DataHandlerFactory
@@ -48,26 +53,56 @@ class Simple(Algorithm):
             return "Nothing"
 
 class YahScraper(Algorithm):
+    # Number of days to calculate change over.
+    period = 2
+
+
     def __init__(self):
         self.URL = "https://finance.yahoo.com/most-active/"
         self.tickers = []
         pass
 
     def run(self):
-        tickers = self.getTickers()
+        self.tickerInfo = defaultdict(dict)
+        
+        self.getTickers()
+        # self.tickerInfo = {
+        #     'AAPL':{},
+        #     'SBUX':{}
+        # }
+
+        self.getTickersData()
+        self.analyseTickers()
+
 
     def getTickers(self):
         res = requests.get(self.URL)
         soup = BeautifulSoup(res.content, "lxml")
         for item in soup.select('.simpTblRow'):
-            # print(item.select('[aria-label=Symbol]')[0].get_text())
-            # print(item.select('[aria-label=Name]')[0].get_text())
-            # print(item.select('[aria-label*=Price]')[0].get_text())
-            # print(item.select('[aria-label=Change]')[0].get_text())
-            # print(item.select('[aria-label="% change"]')[0].get_text())
-            # print(item.select('[aria-label="Market cap"]')[0].get_text())
-            # print(item.select('[aria-label*="Avg vol (3-month)"]')[0].get_text())
-            # print(item.select('[aria-label*="PE ratio (TTM)"]')[0].get_text())
-            # print('____________________________')
-            self.tickers.append(item.select('[aria-label=Symbol]')[0].get_text())
+            symbol = item.select('[aria-label=Symbol]')[0].get_text()
+            self.tickerInfo[symbol] = {}
 
+    def getTickersData(self):
+        end_date = datetime.today().strftime('%Y-%m-%d')
+        start_date = datetime.today() - timedelta(days=YahScraper.period)
+        start_date = start_date.strftime('%Y-%m-%d')
+
+        tickers = list(self.tickerInfo.keys())
+        self.panel_data = data.DataReader(tickers, 'yahoo', start_date, end_date)
+    
+    def analyseTickers(self):
+        currPrices = self.panel_data.iloc[-1]['Adj Close']
+        prevPrices = self.panel_data.iloc[0]['Adj Close']
+        for ticker in self.tickerInfo:
+            change = ((prevPrices[ticker] - currPrices[ticker])/prevPrices[ticker])*100
+
+            self.tickerInfo[ticker]["change"] = change
+            
+            if (change > 0):
+                self.tickerInfo[ticker]["result"] = "BUY"
+            elif (change < 0):
+                self.tickerInfo[ticker]["result"] = "SELL"
+            else:
+                self.tickerInfo[ticker]["result"] = "NONE"
+
+        print(self.tickerInfo)
